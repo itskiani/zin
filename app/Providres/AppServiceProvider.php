fb17b45e3eccca75f3dbf0c42ff3f20adb25c15a
ali <codewithkiani@gmail.com>
2022-09-08 21:57:53 +0430

    write register method AppServiceProvider

--- app/Providres/AppServiceProvider.php
+++ app/Providres/AppServiceProvider.php
@@ -19,6 +19,23 @@ class AppServiceProvider extends AbstractServiceProvider
 
     public function register(): void
     {
-        
+        $container = $this->getContainer();
+        $container->addShared(Router::class, function () use ($container){
+            $strategy = (new ApplicationStrategy())->setContainer($container);
+            return (new Router)->setStrategy($strategy);
+        });
+
+        $container->addShared('response', Response::class);
+        $container->addShared('request', function (){
+            return ServerRequestFactory::fromGlobals(
+                $_SERVER,
+                $_GET,
+                $_POST,
+                $_COOKIE,
+                $_FILES
+            );
+        });
+
+        $container->addShared('emitter', SapiEmitter::class);
     }
 }
