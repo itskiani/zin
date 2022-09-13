bb7ee989eba19dfe4c211761b57a862976bdf072
ali <codewithkiani@gmail.com>
2022-09-13 22:57:02 +0430

    add dependencies into app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,8 @@ use Dotenv\Exception\InvalidPathException;
 
 session_start();
 
+require_once __DIR__.'/../vendor/autoload.php';
+
 try {
     $env = Dotenv::createImmutable(base_path());
     $env->load();
