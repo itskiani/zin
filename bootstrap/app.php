f48e6a36cbc6c3db8a58ea70e5be69161e73dd30
ali <codewithkiani@gmail.com>
2022-09-13 22:55:19 +0430

    add env into app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,1 +1,13 @@
 <?php
+
+use Dotenv\Dotenv;
+use Dotenv\Exception\InvalidPathException;
+
+session_start();
+
+try {
+    $env = Dotenv::createImmutable(base_path());
+    $env->load();
+}catch (InvalidPathException $e){
+    //
+}
