8a6829bfc82eef74ec689a194147f01baccc1c5b
ali <codewithkiani@gmail.com>
2022-09-15 22:42:02 +0430

    configure request and response in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,3 +19,9 @@ require_once base_path('bootstrap/container.php');
 
 $route = $container->get(Router::class);
 require_once base_path('routes/web.php');
+
+$response = $route->dispatch(
+    $container->get('request'), $container->get('response')
+);
+
+return $response;
