b0241e0cb99bf67630c6d1ce2d819dfd7274839d
ali <codewithkiani@gmail.com>
2022-09-20 23:13:29 +0430

    add return type for base_path helper

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,8 @@
 <?php
 
 if(!function_exists('base_path')){
-    function base_path($path=''){
+    function base_path($path=''): string
+    {
         return __DIR__.'/..//'.($path?DIRECTORY_SEPARATOR.$path:$path);
     }
 }
