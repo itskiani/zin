9095e113be9a7e6aaf6378ac66f8e1b9cc418583
ali <codewithkiani@gmail.com>
2022-09-22 22:06:10 +0330

    add index method into HomeController

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,7 +2,16 @@
 
 namespace ItsKiani\Zin\App\controllers;
 
+use Laminas\Diactoros\Response;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
 class HomeController
 {
-
+    public function index(RequestInterface $request) :ResponseInterface
+    {
+        $response = new Response();
+        $response->getBody()->write('home');
+        return $response;
+    }
 }
