717a00412e132842bd079f4ce1af49e1d08eb850
ali <codewithkiani@gmail.com>
2023-07-06 17:41:29 +0330

    Update ViewServiceProvider

--- app/providers/ViewServiceProvider.php
+++ app/providers/ViewServiceProvider.php
@@ -2,7 +2,22 @@
 
 namespace ItsKiani\Zin\App\providers;
 
-class ViewServiceProvider
-{
+use League\Container\ServiceProvider\AbstractServiceProvider;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
+class ViewServiceProvider extends AbstractServiceProvider
+{
+    public function provides(string $id): bool
+    {
+    }
+
+    public function register(): void
+    {
+        $container = $this->getContainer();
+        $loader = new FilesystemLoader(base_path('views'));
+        $twig = new Environment($loader, [
+            'cache' => false
+        ]);
+    }
 }
