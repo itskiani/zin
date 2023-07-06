8f1c5403697ae4261289a5d6f7836a50f00899a6
ali <codewithkiani@gmail.com>
2023-07-06 17:51:59 +0330

    Update View Class

--- app/Views.php
+++ app/Views.php
@@ -2,7 +2,14 @@
 
 namespace ItsKiani\Zin\App;
 
+use Twig\Environment;
+
 class Views
 {
+    protected Environment $twig;
 
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
 }
