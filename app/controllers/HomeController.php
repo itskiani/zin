2730bf5d27ec33d625cbc547e4bd158fece72f10
ali <codewithkiani@gmail.com>
2023-07-10 22:56:03 +0330

    Add construct method in HomeController

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,12 +2,17 @@
 
 namespace ItsKiani\Zin\App\controllers;
 
+use ItsKiani\Zin\App\Views;
 use Laminas\Diactoros\Response;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
 class HomeController
 {
+    public function __construct(
+        protected Views $view
+    ){}
+
     public function index(RequestInterface $request) :ResponseInterface
     {
         $response = new Response();
