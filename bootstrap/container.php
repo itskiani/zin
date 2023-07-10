1eab4b7bbb09660bf2b1ae544fd5dedd60f803ba
ali <codewithkiani@gmail.com>
2023-07-10 23:02:19 +0330

    Update container file

--- bootstrap/container.php
+++ bootstrap/container.php
@@ -1,6 +1,14 @@
 <?php
 
 $container = new League\Container\Container();
+
+$container->delegate(
+    new League\Container\ReflectionContainer()
+);
+
 $container->addServiceProvider(
     new ItsKiani\Zin\App\providers\AppServiceProvider()
+);
+$container->addServiceProvider(
+    new ItsKiani\Zin\App\providers\ViewServiceProvider()
 );
